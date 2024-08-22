700bf5541d51ec886438eaa520ec5a1f9cd5c33a
Codekilaa <xluixarturox123@gmail.com>
2024-08-22 10:29:14 -0500

    Subiendo cambios en portafolio

--- portafolio/views/tech_stack.py
+++ portafolio/views/tech_stack.py
@@ -6,7 +6,7 @@ from portafolio.styles.styles import EmSize, Size
 
 def tech_stack(technologies: list[Technology]) -> rx.Component:
     return rx.vstack(
-        heading("Tecnologías"),
+        heading("Tecnologías y Herramientas"),
         rx.flex(
             *[
                 rx.badge(
